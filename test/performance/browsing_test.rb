34b0af11c2332ce53b1fc221bbe2e319289c4e7a
divine13 <dubedivine@gmail.com>
2013-07-19 21:54:41 +0200

    project one

--- test/performance/browsing_test.rb
+++ test/performance/browsing_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 require 'rails/performance_test_help'
-
+#this is a comment in ror
 class BrowsingTest < ActionDispatch::PerformanceTest
   # Refer to the documentation for all available options
   # self.profile_options = { :runs => 5, :metrics => [:wall_time, :memory]
